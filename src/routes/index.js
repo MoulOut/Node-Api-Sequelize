1dee08614ed11ad6b5c546b5f74536396a8d301a
MoulOut <mmoulinlima@outlook.com>
2024-01-11 13:38:48 -0300

    new routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const pessoas = require('./peopleRoutes.js');
+const courses = require('./courseRoutes.js');
+const categories = require('./categorieRoutes.js');
+const registrations = require('./registrationRoutes.js');
 
 module.exports = (app) => {
-  app.use(express.json(), pessoas);
+  app.use(express.json(), pessoas, courses, categories, registrations);
 };
